4f40ad7c1b6b9f7efd76d0d6c019556300f1161f
iamlogan <iamlogandavidson@gmail.com>
2022-01-14 12:12:35 +0000

    Reinstalled Bootstrap, rebuild static files

--- expensessite/settings.py
+++ expensessite/settings.py
@@ -128,3 +128,8 @@ LOGOUT_REDIRECT_URL = "login"
 
 # Custom user model
 AUTH_USER_MODEL = 'expensesapp.User'
+
+# For sending emails in development
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'localhost'
+# EMAIL_PORT =
